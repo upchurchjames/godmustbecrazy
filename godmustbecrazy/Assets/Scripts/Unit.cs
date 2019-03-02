c649965ccdccede18510d8a8cee41486484cd89c
Travis Kerr <kerrta@users.noreply.github.com>
2019-03-02 01:42:04 -0500

    Added unit class

---
+++ godmustbecrazy/Assets/Scripts/Unit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public float Health;
+    public float Strength;
+    public float MovementSpeed;
+    public MeshFilter Mesh;
+    public GameObject Object;
+
+    // Awake is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateHealth(float changeAmount)
+    {
+        Health += changeAmount;
+
+        if (Health > 100.0f)
+            Health = 100.0f;
+
+        if (Health <= 0)
+            Die();
+    }
+
+    void Die()
+    {
+
+    }
+}
