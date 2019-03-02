012b65ddfb592a665e061aeb31b3194ffc53ca02
James <jamesgupchurch@knights.ucf.edu>
2019-03-02 03:53:44 -0500

    Enemy classes

---
+++ godmustbecrazy/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Unit
+{
+    public enum EnemyType
+    {
+        Seraph,
+        Angel,
+        JesusChrist
+    }
+    
+    public EnemyType type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = 100;
+        Strength = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    EnemyType UpgradeEnemy(EnemyType type)
+    {
+        return type;
+    }
+}
---
+++ godmustbecrazy/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public float spawnTime = 5f;
+    public Enemy enemy;
+    public Transform[] spawnPoints;
+    public int numEnemy1, numEnemy2, numEnemy3;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
